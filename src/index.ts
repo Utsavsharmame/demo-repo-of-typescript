cfe1110ae7dba88849380cb36f85f8008b75412f
Utsavsharmame <sutsav237@gmail.com>
2025-01-03 22:10:19 +0530

    Updated index.ts with new code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,61 @@
 
+/*interface User {
+    name: string;
+    id: string;  
+    age: number;
+    email:string;
+    password:string;
 
-let greet = () => {
-    console.log("Hello, World!");
 };
+
+type UpdateProps = Pick<User, 'name' | 'id' | 'age'>;
+
+function updateUser(updateProps: UpdateProps) {
+    console.log(updateProps);
+}
+
+
+
+
+
+const a = [1,2,3];
+a[2] = 4;
+
+
+// readonly 
+type User = {
+     name: string;
+     id: string;
+
+}
+
+const user: Readonly<User>= {
+    name: 'John',
+    id: '123'
+}
+
+user.id = '456'; // Error: Cannot assign to 'id' because it is a read-only property.
+
+
+record and maps:-
+
+
+type User = {
+    name: string;
+    age: number;
+    email: string;
+}
+
+const users = new Map<string, User>()
+users.set("ras@qd1", { name: "Ras", age: 25, email: "ras@qd1"})
+users.set("sara@qd1", { name: "Sara", age: 25, email: "sara@qd1"})
+
+const user = users.get("ras@qd1")
+console.log(user);
+
+*/
+
+//exclude:-
+
+
+
